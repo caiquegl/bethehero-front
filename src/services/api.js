3e2c9d377973b542b46fdcc505e58d734124a223
caiquegl <arq.caique@hotmail.com>
2020-04-24 18:09:28 -0300

    teste

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL: 'https://vigilant-varahamihira-f767e8.netlify.app/'
 });
 
 export default api;
